f32c457da32d753684c334ebb724ccf886de7667
CarlosD-1699 <carlos.dbarbosa0716@gmail.com>
2023-07-18 11:19:42 -0500

    feat: added links to navigation bar icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,8 @@ import { BiHomeAlt, BiUser } from "react-icons/bi";
 import {
   BsClipboardData,
   BsBriefcase,
-  BsChatSquare,
   BsFillClipboardDataFill,
+  BsChatSquareText,
 } from "react-icons/bs";
 // Link
 import { Link } from "react-scroll";
@@ -15,20 +15,53 @@ const Nav = () => {
     <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
       <div className="container mx">
         <div
-          className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex items-center 
-        justify-between text-2xl text-white/50"
+          className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto 
+          px-5 flex items-center justify-between text-2xl text-white/50"
         >
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="home"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BiHomeAlt />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="about"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BiUser />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="services"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BsClipboardData />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="work"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BsBriefcase />
+          </Link>
+          <Link
+            to="contact"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BsChatSquareText />
           </Link>
         </div>
       </div>
